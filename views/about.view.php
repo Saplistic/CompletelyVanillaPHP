b63f9d4e3a3542b1e0ee8c1e5ec4b7eee2c29e46
Saplistic <167242143+Saplistic@users.noreply.github.com>
2024-09-05 22:55:10 +0200

    Initial commit

---
+++ views/about.view.php
@@ -0,0 +1,9 @@
+<?php require("parts/head.view.php") ?>
+
+<?php require("parts/header.view.php") ?>
+
+<main>
+    <h1>About</h1>
+</main>
+
+<?php require("parts/footer.view.php") ?>
---
+++ views/contact.view.php
@@ -0,0 +1,9 @@
+<?php require("parts/head.view.php") ?>
+
+<?php require("parts/header.view.php") ?>
+
+<main>
+    <h1>Contact</h1>
+</main>
+
+<?php require("parts/footer.view.php") ?>
