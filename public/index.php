9b04c92b88fb84ab3bd6dd71784330b437760b7f
Saplistic <167242143+Saplistic@users.noreply.github.com>
2024-09-15 19:06:53 +0200

    Created an autoloader

--- public/index.php
+++ public/index.php
@@ -1,12 +1,15 @@
-<?php 
+<?php
 
 const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'functions.php';
 
-require base_path('Response.php');
+spl_autoload_register(function ($class) {
 
-require base_path('Database.php');
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    
+    require base_path("{$class}.php");
+});
 
 require base_path('router.php');
 
